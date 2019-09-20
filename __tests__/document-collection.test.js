06717054965d01cf8f10a422aa29259f34717108
allisonbusse <allisonbusse@gmail.com>
2019-09-19 17:33:54 -0700

    get all function

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -7,8 +7,7 @@ jest.mock('../lib/files.js', () => ({
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
 const DocumentCollection = require('../lib/document-collection');
-const documentNew = new DocumentCollection;
-const shortid = require('shortid');
+const documentNew = new DocumentCollection('document');
 
 
 describe('Document Collection', () => {
@@ -23,7 +22,7 @@ describe('Document Collection', () => {
     //act
     return documentNew.save(objectExample)
       .then(() => {
-        const dest = `./${objectExample.id}.json`;
+        const dest = `./document/${objectExample.id}.json`;
         const writeCalls = writeFile.mock.calls;
         expect(writeCalls.length).toBe(1);
         expect(writeCalls[0][0]).toBe(dest);
@@ -34,7 +33,6 @@ describe('Document Collection', () => {
   it(`propagates error`, () => {
     // arrange
     // const objectExample = { name: 'Allison' };
-    const dest = `./${objectExample.id}.json`;
 
     const error = 'file error';
     writeFile.mockRejectedValueOnce(error);
@@ -49,10 +47,8 @@ describe('Document Collection', () => {
 
   it('gets a file by id', () => {
     // arrange 
-    // const objectExample = { name: 'Allison' };
-    const source = `./${objectExample.id}.json`;
+    const source = `./document/${objectExample.id}.json`;
     readFile.mockResolvedValue(objectExample);
-
 
     //act
     return documentNew.get(objectExample.id)
@@ -61,6 +57,29 @@ describe('Document Collection', () => {
         expect(readCalls.length).toBe(1);
         expect(readCalls[0][0]).toBe(source);
       });
+  });
+
+
+  it('gets all files in a directory', () => {
+    // arrange 
+    const objectExampleTwo = { name: 'Allison', id: 'lit' };
+
+    const readPromise = Promise.resolve(JSON.stringify(objectExampleTwo));
+    readFile.mockReturnValueOnce(readPromise);
+
+    const readdirPromise = Promise.resolve(['lit.json']);
+    readdir.mockReturnValueOnce(readdirPromise);
+
+
+    //act
+    return documentNew.getAll()
+      .then(array => {
+        expect(readdir.mock.calls[0][0]).toBe('document');
+        expect(readFile.mock.calls[1][0]).toBe('./document/lit.json');
+        expect(array[0].id).toBe(objectExampleTwo.id);
+      });
+
+    
   });
 
 });
