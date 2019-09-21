785d934d3271a3461874d6dc648e7f51c6e96085
allisonbusse <allisonbusse@gmail.com>
2019-09-20 17:03:17 -0700

    index.js testing

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-// const DocumentCollection = require('./lib/document-collection');
+const DocumentCollection = require('./lib/document-collection');
 
-// const documents = new DocumentCollection(/* path to use */);
+const documents = new DocumentCollection('./documents');
+const objExample = { name: 'Allison' };
+documents.save(objExample)
+  .then(objExample => {
+    console.log(objExample);
+    documents.get(objExample.id)
+      .then(returnedObjects => {
+        console.log(returnedObjects);
+      })
+      .then(() => {
+        documents.getAll()
+          .then(res => {
+            console.log(res);
+          });
+      });
+  }
+  );
 
-// write some code to exercise your document collection
+
